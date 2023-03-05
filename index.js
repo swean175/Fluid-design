a5b6af9fd12cb5069197739bb4e67ed7a0425e82
Damian <swean@vp.pl>
2023-03-05 18:37:50 +0100

    font
    adjusting

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-
 let anim = false
 let one = 20
 let two = 200
@@ -9,28 +7,27 @@ render()
 
 
 function add(){
+
   if (one > 50){
     sub()
-    // change()
-  } else {
-one += 1 
-two -= 1
+    } else {
+    one += 1 
+     two -= 1
 
 render()
-  }
-}
+
+}}
 
 function sub(){
   if (one < 10){
-    add()
+  add()
   } else {
- one -= 1
-two += 1
+   one -= 1
+   two += 1
 
 render()
-  }
- 
-}
+
+}}
 
 function change(){
   anim = !anim
@@ -42,21 +39,18 @@ function change(){
 
 function offset(){
 
-    if (anim === true ){
-sub()
-console.log(one)
+  if (anim === true ){
+    sub()
     } else if (anim === false)
     {
      add()
-    console.log(one)
     }
-   
 }
 
 function check(){
-  if (one === 50){
+    if (one === 50){
     anim = true
-  } else if (one === 10){
+     } else if (one === 10){
     anim = false
   }
 }
@@ -64,25 +58,21 @@ function check(){
 
 function  render(){
   check()
-console.log(anim)
-setTimeout(offset, 100)
+  setTimeout(offset, 100)
 
   const blob = document.querySelector('#blob').innerHTML =  ` 
     <radialGradient id="RadialGradient1">
       <stop offset="${one}%" stop-color="#271F1F" />
       <stop offset="${two}" stop-color="#6a6767" />
-    </radialGradient>
- 
-
-`
+    </radialGradient>`
 }
 
 
-// let el = document.getElementById('turbulence');
-// tl = new TimelineMax({paused:true});
+let el = document.getElementById('turbulence');
+tl = new TimelineMax({paused:true});
 
-// tl.from('div', 1,{opacity:0, y: -70});
-// // tl.staggerFrom('#gen', 1, {opacity: 0, x:-1, y:-1, ease: Power4.easeOut}, 0.1)
-// tl.to(el, 1.5, {attr:{baseFrequency: '1 1'}}, 0)
-// tl.play();
+tl.from('div', 1,{opacity:0, y: -70});
+tl.staggerFrom('#gen', 1, {opacity: 0, x:-1, y:-1, ease: Power4.easeOut}, 0.1)
+tl.to(el, 1.5, {attr:{baseFrequency: '1 1'}}, 0)
+tl.play();
 
